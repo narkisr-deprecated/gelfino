fc098f359736273073f58179e48783d0c63f0d4e
narkisr <narkisr@gmail.com>
2012-01-27 04:17:19 +0200

    Moving to latest Aleph alpha and clojure 1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject gelfino "1.0.0-SNAPSHOT"
   :description "An extendable Gelf server"
-  :dependencies [[org.clojure/clojure "1.2.1"] [aleph "0.2.0"]]
-  )
+  :dependencies [[aleph "0.2.1-alpha2-SNAPSHOT"] 
+                 [org.clojure/clojure "1.3.0"]
+                 [org.clojure/core.match "0.2.0-alpha9"] ])
