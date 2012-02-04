ab4e2144039db11d9bb3e1fe50652b7401cda1a2
narkisr <narkisr@gmail.com>
2012-02-04 04:24:31 +0200

    filter-fn works

--- src/gelfino/streams.clj
+++ src/gelfino/streams.clj
@@ -1,4 +1,29 @@
-(ns gelfino.stream)
+(ns gelfino.streams
+  (:require [gelfino.core :as gelfino-c]))
 
+(defn into-pred [selector]
+  (cond 
+    (instance? clojure.lang.IFn selector) selector
+    (instance? java.util.regex.Pattern selector) (fn [v] (-> (re-matches selector v) nil? not))
+    (instance? java.lang.String selector) (fn [v] (.contains v selector))
+    :else (throw (Exception.(str "Bad predicate type " selector)))))
 
-(defmacro stream [name filter action])
+(defn filter-fn [pred-pairs]
+  (let [pairs  (map #(into [] % ) (partition 2 pred-pairs))
+        preds (map (fn [[k v]] [k (into-pred v)]) pairs)]
+     (fn [m]
+      (every? (fn [[k v]] (v (m k))) preds))))
+
+(defmacro defstream
+  "A stream of messages filtered out of the entire messages recieved 
+   the defenition takes pairs of key values where key is the message part we filter upon and the value is either:
+    * A predicate function that accepts the part and returns true if it macthes.
+    * A regex on which the parts will be matched.
+    * A substring of the matched message part." 
+  [name & rest]
+      
+  )
+
+;examples
+(defstream not-too-long :short-message #" not too long " 
+     (println message))
--- test/gelfino/test/streams.clj
+++ test/gelfino/test/streams.clj
@@ -6,8 +6,13 @@
       (is (= (into-pred empty-fn) empty-fn))
       (is (= ((into-pred regex) "foo") true))
       (is (= ((into-pred string) "barbar") true))
-      
+      (is (thrown? java.lang.Exception (into-pred 1))) 
       ))
+
+(deftest filtering 
+  (let [messages [{:short-message "foo"} {:short-message "bar"} {:short-message "hello"}]]
+    (is (= (filter (filter-fn [:short-message "fo"]) messages) '({:short-message "foo"})))         
+    (is (= (filter (filter-fn [:short-message #"foo"]) messages) '({:short-message "foo"})))))
 
 (deftest simple-stream
   (defstream not-too-long :short-message #" not too long " (println message))     
