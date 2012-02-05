8aa95fac565464c8e551f7e18ae1af627c4b2e1d
narkisr <narkisr@gmail.com>
2012-02-05 04:35:17 +0200

    adding example project

---
+++ example/project.clj
@@ -0,0 +1,6 @@
+(defproject gelfino-example "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[com.narkisr/gelfino "0.1.0"] 
+                 [org.clojure/clojure "1.3.0"]
+                 [cljs-uuid "0.0.2"]
+                 [org.clojars.tavisrudd/redis-clojure "1.3.1-SNAPSHOT"]])
---
+++ example/test/gelfino_example/test/core.clj
@@ -0,0 +1,6 @@
+(ns gelfino-example.test.core
+  (:use [gelfino-example.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
