93a92437d0d2ecb8d3ecce325d541e820785a8f9
narkisr <narkisr@gmail.com>
2012-02-12 04:01:59 +0200

    adding chunk stats

--- src/gelfino/statistics.clj
+++ src/gelfino/statistics.clj
@@ -3,13 +3,14 @@
    [clojure.tools.logging :only (trace info debug warn)]
    tron))
 
-(def counters (agent {:processed 0 :prev 0 :recieved 0}))
+(def counters (agent {:processed 0 :prev 0 :recieved 0 :chunks 0}))
 
 (defn collect [] 
   (tron/do-periodically 5000 
-    (let [{:keys [processed prev recieved]} @counters]
+    (let [{:keys [processed prev recieved chunks]} @counters]
       (info (str "messages recieved: " recieved))
       (info (str "messages processed: " processed))
+      (info (str "chunks processed: " chunks))
       (info (str "current rate is: " (- processed prev)))
       (send counters #(assoc % :prev (% :processed ))))))
 
@@ -21,3 +22,6 @@
 
 (defn inc-received [] 
   (send counters #(assoc % :recieved (-> % :recieved (+ 1)))))
+
+(defn inc-chunks [] 
+  (send counters #(assoc % :chunks (-> % :chunks (+ 1)))))
