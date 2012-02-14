4b97691cfecc5274c686e177f332af9cb985f4a5
narkisr <narkisr@gmail.com>
2012-02-14 03:21:47 +0200

    Adding esper

---
+++ src/gelfino/esper.clj
@@ -0,0 +1,29 @@
+(ns gelfino.esper
+ (:use [clojure.walk :only (stringify-keys) ])
+ (:import 
+    java.util.Date
+    (com.espertech.esper.client Configuration EPServiceProviderManager UpdateListener))) 
+
+(def esper-env (atom {:provider nil :runtime nil :config nil :admin nil}))
+
+(defn initialize-esper []
+  "A nice macro could be to do all this self referencing 
+   {:provider (EPServiceProviderManager/getProvider :config)}"
+  (swap! esper-env assoc :config (Configuration.)) 
+  (swap! esper-env assoc :provider (EPServiceProviderManager/getProvider "gelfino" (@esper-env :config)))
+  (swap! esper-env assoc :runtime (.getEPRuntime (@esper-env :provider)))
+  (swap! esper-env assoc :admin (.getEPAdministrator (@esper-env :provider))))
+
+(defrecord UnicornHandler [l]
+     UpdateListener
+      (update [this new-events old-events] (l new-events old-events)))
+
+(defn add-event [name e]
+  (.addEventType (@esper-env :config) name (stringify-keys e)))
+
+(defn add-query [query listener ]
+  (.addListener (.createEPL (@esper-env :admin) query) (UnicornHandler. listener)))
+ 
+(initialize-esper)
+(add-event "UnicornSeen"{:time Date :name "string"})
+(.sendEvent runtime (stringify-keys {:time (Date.) :name "Im a cute uni!"}) "UnicornSeen");
