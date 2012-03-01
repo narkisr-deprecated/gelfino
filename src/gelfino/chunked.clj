596bd56e36c194b0963d915a214148e17bfc708e
narkisr <narkisr@gmail.com>
2012-03-01 03:22:53 +0200

    moving from channel to sorted maps since messages might arrive out of order

--- src/gelfino/chunked.clj
+++ src/gelfino/chunked.clj
@@ -6,39 +6,29 @@
     lamina.core 
     (gelfino constants header)))
 
-(def channels (ref {}))
+(def buffers (ref {}))
 
 (defn- merge-bytes [^ByteArrayOutputStream output ^bytes curr]
     (.write output curr chunked-header-length (- (alength curr) chunked-header-length))
       output)
 
-(defn merge-chunks [chunks out-channel]
-  (let [result (reduce* merge-bytes (ByteArrayOutputStream.) chunks)]
-    (on-success result (fn [^ByteArrayOutputStream output] (enqueue out-channel (.toByteArray output)))) 
-    (on-error result #(error %))))
-
-(defn add-channel [out-channel id]
- (let [ch (channel)]
-   (alter channels assoc id 
-     {:channel ch 
-      :result (run-pipeline ch
-                #(reduce* merge-bytes (ByteArrayOutputStream.) %)
-                #(enqueue out-channel (.toByteArray %)))})))
+(defn update-buf [id buf s m]
+  (alter buffers assoc id (assoc buf s m)))
 
 (defn handle-chunked [^bytes m output]
   "Handling chunked messages, output is the channel onto completed messages will be written"
   {:pre [(> (alength m) chunked-header-length)] }  
-    (let [{:keys [id sequence total]} (chunked-header m)]
-      (debug id)
-      (dosync
-        (when-not (contains? @channels id)
-          (add-channel output id)))
-      (enqueue (get-in @channels [id :channel]) m)
-      (stats/inc-chunks)
-      (when (= sequence (- total 1))
-        (future
-          (close (get-in @channels [id :channel]))
-          (deref (get-in @channels [id :result])) 
-          ; if dosync is out of future scope we have a race condition!
-          (dosync 
-            (alter channels dissoc id))))))
+  (let [{:keys [id sequence total]} (chunked-header m)]
+    (debug id)
+    (dosync
+      (if-not (contains? @buffers id)
+        (update-buf id (sorted-map) sequence m) 
+        (update-buf id (buffers id) sequence m)))
+    (stats/inc-chunks)
+    (when (= (-> (buffers id) vals count) total)
+      (future
+        (let [merged (reduce merge-bytes (ByteArrayOutputStream.) (vals (buffers id)))]
+          (enqueue output (.toByteArray merged)))
+        ; if dosync is out of future scope we have a race condition!
+        (dosync 
+          (alter buffers dissoc id))))))
