7b7d70d8ab533edddb3ab7b1c2bd9725a9eb07b7
narkisr <narkisr@gmail.com>
2012-03-01 03:21:44 +0200

    indentation

--- src/gelfino/compression.clj
+++ src/gelfino/compression.clj
@@ -1,27 +1,27 @@
 (ns gelfino.compression
- (:import (java.io ByteArrayOutputStream ByteArrayInputStream) (java.util.zip InflaterInputStream GZIPInputStream)))
+  (:import (java.io ByteArrayOutputStream ByteArrayInputStream) (java.util.zip InflaterInputStream GZIPInputStream)))
 
 
 (defn decompress-zlib [data]
   (let [buffer (byte-array (alength data))
         out (ByteArrayOutputStream.)
         in (InflaterInputStream. (ByteArrayInputStream. data)) ]
-       (loop [bytes-read 0]
-         (when-not (< bytes-read 0)
-           (.write out buffer 0 bytes-read)
-           (recur (.read in buffer))))
-       (.flush out)
-       (.close out)
-       (String. (.toByteArray out) "UTF-8")))
+    (loop [bytes-read 0]
+      (when-not (< bytes-read 0)
+        (.write out buffer 0 bytes-read)
+        (recur (.read in buffer))))
+    (.flush out)
+    (.close out)
+    (String. (.toByteArray out) "UTF-8")))
 
 (defn decompress-gzip [data]
   (let [buffer (byte-array (alength data))
         out (ByteArrayOutputStream.)
         in (GZIPInputStream. (ByteArrayInputStream. data)) ]
-       (loop [bytes-read 0]
-         (when-not (< bytes-read 0)
-           (.write out buffer 0 bytes-read)
-           (recur (.read in buffer))))
-       (.flush out)
-       (.close out)
-       (String. (.toByteArray out) "UTF-8")))
+    (loop [bytes-read 0]
+      (when-not (< bytes-read 0)
+        (.write out buffer 0 bytes-read)
+        (recur (.read in buffer))))
+    (.flush out)
+    (.close out)
+    (String. (.toByteArray out) "UTF-8")))
