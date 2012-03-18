e442f43e5007e29e6f402a33224ca7f57b693453
narkisr <narkisr@gmail.com>
2012-03-18 05:08:22 +0200

    adding main class

--- example/project.clj
+++ example/project.clj
@@ -1,6 +1,9 @@
-(defproject gelfino-example "1.0.0-SNAPSHOT"
+(defproject gelfino-example "1.0"
   :description "FIXME: write description"
   :dependencies [[com.narkisr/gelfino "0.2.0"] 
                  [org.clojure/clojure "1.3.0"]
                  [cljs-uuid "0.0.2"]
-                 [org.clojars.tavisrudd/redis-clojure "1.3.1-SNAPSHOT"]])
+                 [org.clojars.tavisrudd/redis-clojure "1.3.1-SNAPSHOT"]]
+  :main gelfino.example.core 
+  
+  )
