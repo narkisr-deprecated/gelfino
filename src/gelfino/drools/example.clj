4cf952ddde58e4343e25a23fb075c9f47a82968a
narkisr <narkisr@gmail.com>
2012-03-25 05:18:26 +0200

    a more complex example

--- src/gelfino/drools/example.clj
+++ src/gelfino/drools/example.clj
@@ -2,12 +2,15 @@
   (:use gelfino.drools.bridging gelfino.drools.straping)
   (:import [gelfino.drools.bridging Message]))
 
+(defn now [] (java.util.Date.))
+
 (let [session (build-session-from-drl "src/main/resources/example.drl") 
       entry (.getWorkingMemoryEntryPoint session "entryone")]
-  (dosync
-    (alter actions assoc "rule1" #(println "rule 1 fired me")))
+  (dosync (alter actions assoc "rule1" #(println "rule fired me")))
   (.setGlobal session "actions" actions)
-  (.insert entry (Message. "INFO" ""))
-  (.insert entry (Message. "bla" ""))
+  ;(.insert entry (Message. "INFO" (now)))
+  ;(.insert entry (Message. "bla" (now)))
+  ;(.insert entry (Message. "bla" (now)))
+  ;(.insert entry (Message. "INFO" (java.util.Date.)))
   (.fireAllRules session))
 
