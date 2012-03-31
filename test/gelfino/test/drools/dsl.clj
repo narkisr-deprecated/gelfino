a37130cedd6ba5a65e8d769be7fde8bcbebca5e2
narkisr <narkisr@gmail.com>
2012-04-01 00:32:26 +0300

    introducing lhs test ns

--- test/gelfino/test/drools/dsl.clj
+++ test/gelfino/test/drools/dsl.clj
@@ -1,6 +1,6 @@
 (ns gelfino.test.drools.dsl
   (:import [gelfino.drools.bridging Message])
-  (:use clojure.pprint clojure.test 
+  (:use clojure.test 
         gelfino.drools.dsl gelfino.drools.bridging 
         gelfino.drools.straping clojure.java.data))
 
@@ -26,7 +26,6 @@
 
 (def rules-map (from-java (first (-> infos (.getRules)))))
 
-
 (deftest static-rhs 
   (let [{consequence :consequence } rules-map {action "infos"} @actions]
     (is (= "actions.deref().get(\"infos\").invoke();\n" consequence))
@@ -44,18 +43,4 @@
         {[{exp :expression}]:descrs} constraint]
     (is (= "level==\"INFO\"" exp))
     (is (= "event-stream" entry))))
-
-(deftest simple-lhs 
-   (is (= (lhs '(when $message :> Message (== level "INFO" ) :from (entry-point "event-stream")))
-         "$message:Message(level==\"INFO\") from entry-point \"event-stream\" ")))
-
-(deftest complex-lhs 
-  (is (= (lhs '(when Number (> intValue 3) :from 
-                (accumulate $message :> Message (== level "INFO") :over (window :time 1 m)
-                  :from (entry-point event-stream)  (count $message))))
-    "Number(intValue>3) from accumulate($message:Message(level==\"INFO\") over window:time(1m) from entry-point \"event-stream\" count($message)) ")))
-
-; Number(intValue > 3) from accumulate($message:Message(level == "INFO") over window:time(1m) from entry-point entryone, count($message))
-; ((Number intValue > 3) :from (accumulate $message :of-type Message (== level "INFO") :over (window:time 1m) :from entry-point entryone  (count $message)))
-
 
---
+++ test/gelfino/test/drools/lhs.clj
@@ -0,0 +1,14 @@
+(ns gelfino.test.drools.lhs
+  (:use clojure.test gelfino.drools.dsl))
+
+(deftest simple-lhs 
+   (is (= (lhs '(when $message :> Message (== level "INFO" ) :from (entry-point "event-stream")))
+         "$message:Message(level==\"INFO\") from entry-point \"event-stream\" ")))
+
+(deftest complex-lhs 
+  (is (= (lhs '(when Number (> intValue 3) :from 
+                (accumulate $message :> Message (== level "INFO") :over (window :time 1 m)
+                  :from (entry-point event-stream)  (count $message))))
+    "Number(intValue>3) from accumulate($message:Message(level==\"INFO\") over window:time(1m) from entry-point \"event-stream\" count($message)) ")))
+
+
