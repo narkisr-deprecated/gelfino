28ad579422c997bbe7a87ae64a8e7faf2d2091b2
narkisr <narkisr@gmail.com>
2012-10-14 06:16:27 +0200

    flattening event to tuple

--- src/gelfino/storm/spout.clj
+++ src/gelfino/storm/spout.clj
@@ -1,13 +1,21 @@
 (ns gelfino.storm.spout
   (:use clojure.pprint
+        [gelfino.client :only (message-template)]
         [backtype.storm clojure config]  
         [gelfino streams bootstrap]))
 
+(def tuple-keys (sort (keys message-template)))
+
+(defn tuple-form []
+  (into [] (map (comp str name) tuple-keys)))
+
+(defn into-tuple [event] 
+  (into [] (map #(% event) tuple-keys)))
 
 (defmacro defgspout [name]
   `(do 
      (def ~'events (agent clojure.lang.PersistentQueue/EMPTY))
-     (defspout ~name ["event"] {:prepare true}
+     (defspout ~name ~(tuple-form) {:prepare true}
         [conf# context# collector#]
         (start-processing "0.0.0.0" "12201")
         (~'spout
@@ -15,8 +23,8 @@
              (if-let [event# (peek @~'events)]
                (do 
                  (send ~'events pop)
-                 (emit-spout! collector# [event#]))))
+                 (emit-spout! collector# (into-tuple event# )))))
           (~'ack [id#])))) 
   )  
-;(pprint (macroexpand-1 '(defgpout foo))) 
+
 
